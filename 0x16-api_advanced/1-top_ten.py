1fd56550a1bb935e489e7527c18878abed65a1d4
Hope_Davids <hopedavids@gmail.com>
2023-06-06 17:10:46 +0000

    fixed the bug in the code

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-""" 
+"""
 Queries the reddit API and returns the top 10 hot posts in the subreddit
 
 """
 
-import requests, json
+
+import requests
+import json
 
 
 def top_ten(subreddit):
-    """ This method queries the reddit API and returns the list of top 10 
+    """ This method queries the reddit API and returns the list of top 10
         subreddit
     """
     uri = 'https://www.reddit.com/r/{}/hot.json?show="all"&limit=10'.format(
         subreddit)
-     headers = {'User-Agent': 'Python/1.0(Holberton School 0x16)'}
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16)'}
     response = requests.get(uri, headers=headers)
     try:
         top_ten = response.json()['data']['children']
