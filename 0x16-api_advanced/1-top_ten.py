c12469f843b6b737cfb29ee4a1bedd1ba9ae2249
Hope_Davids <hopedavids@gmail.com>
2023-06-06 17:05:54 +0000

    query the api to list the top ten hot post

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" 
+Queries the reddit API and returns the top 10 hot posts in the subreddit
+
+"""
+
+import requests, json
+
+
+def top_ten(subreddit):
+    """ This method queries the reddit API and returns the list of top 10 
+        subreddit
+    """
+    uri = 'https://www.reddit.com/r/{}/hot.json?show="all"&limit=10'.format(
+        subreddit)
+     headers = {'User-Agent': 'Python/1.0(Holberton School 0x16)'}
+    response = requests.get(uri, headers=headers)
+    try:
+        top_ten = response.json()['data']['children']
+        for post in top_ten:
+            print(post['data']['title'])
+    except KeyError:
+        print("None")
